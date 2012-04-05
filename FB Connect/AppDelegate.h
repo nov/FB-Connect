aa576b21792ec2ff2c01ac727f1fdd2094bfee24
nov matake <nov@matake.jp>
2012-04-05 22:07:27 +0900

    "replace_me" as access token

--- FB Connect/AppDelegate.h
+++ FB Connect/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define VICTIMS_FB_ACCESS_TOKEN @"AAAByykUMsVsBAN4FfSm3vAJgoL8GozL4dDaZBzQTYLACn6usmj1oruAYAY4ad9ZCrYgNaLTZCibAHZC9jWYveJs35iJEtdMSbJvoZAX9ZAtwZDZD"
+#define VICTIMS_FB_ACCESS_TOKEN @"replace_me"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
